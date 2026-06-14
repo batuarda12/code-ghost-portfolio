d7483812a9d7afb2687639916137c6d29235c954
Batuhan Arda Ünal <batuarda12@gmail.com>
2026-06-14 17:18:34 +0300

    ilk kurulum: vite + react + tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-[#040d1a] min-h-screen text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-[#00f0ff]">CODE_GHOST 👻</h1>
+    </div>
+  )
+}
+
+export default App
